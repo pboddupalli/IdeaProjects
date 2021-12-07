e9eb0881992c064435b83c704b1b18d5093fdef5
Prasad Boddupalli <pboddupalli@mapr.com>
2021-12-07 19:16:27 +0530

    first checkin

---
+++ leetcode/src/tutorials/lists.java
@@ -0,0 +1,7 @@
+package tutorials;
+
+public class lists {
+    public static void main(String[] args) {
+        System.out.println("back to java");
+    }
+}
---
+++ leetcode/src/tutorials/maps.java
@@ -0,0 +1,11 @@
+package tutorials;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class maps {
+  public static void main(String[] args) {
+    Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+    System.out.println("Hello maps in java");
+  }
+}
